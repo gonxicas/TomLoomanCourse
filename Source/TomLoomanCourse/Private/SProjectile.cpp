5ff8b47454e0ba17a7c3276fca2bc4f9a64a5548
Gonzalo Casin <gcasinbricio@hotmail.com>
2024-04-22 19:16:55 +0200

    fix(projectiles): fix header include oreder

--- Source/TomLoomanCourse/Private/SProjectile.cpp
+++ Source/TomLoomanCourse/Private/SProjectile.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "SProjectile.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Particles/ParticleSystemComponent.h"
-#include "SProjectile.h"
 
 // Sets default values
 ASProjectile::ASProjectile()
--- Source/TomLoomanCourse/Private/STeleportProjectile.cpp
+++ Source/TomLoomanCourse/Private/STeleportProjectile.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Particles/ParticleSystemComponent.h"
 #include "STeleportProjectile.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 
