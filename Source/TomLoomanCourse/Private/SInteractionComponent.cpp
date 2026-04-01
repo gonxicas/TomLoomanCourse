cb222f52c7e07175cef8a65a6a8bd706a1622da2
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-04-01 14:54:37 +0900

    feat(SInteractionComponent): create UI widget when focusing on an interactable

--- Source/TomLoomanCourse/Private/SInteractionComponent.cpp
+++ Source/TomLoomanCourse/Private/SInteractionComponent.cpp
@@ -1,84 +1,111 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "SInteractionComponent.h"
 #include "SGameplayInterface.h"
+#include "Blueprint/UserWidget.h"
+#include "SWorldUserWidget.h"
 
 static TAutoConsoleVariable<bool> CVarDebugDrawInteractions(TEXT("su.DrawDebugInteraction"), false,
                                                             TEXT("Enable debug lines for Interact Component."),
                                                             ECVF_Cheat);
 
-// Sets default values for this component's properties
 USInteractionComponent::USInteractionComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	
+	TraceDistance = 500.f;
+	TraceRadius = 30.f;
+	CollisionChannel = ECC_WorldDynamic;
 }
 
 
-// Called when the game starts
 void USInteractionComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 }
 
 
-// Called every frame
 void USInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType,
                                            FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	FindBestInteractable();
 }
 
-void USInteractionComponent::PrimaryInteract()
+void USInteractionComponent::FindBestInteractable()
 {
-
 	bool bDebugDraw = CVarDebugDrawInteractions.GetValueOnGameThread();
-	
-	// FHitResult Hit;
+
 	FVector End;
 
 	FVector EyeLocation;
 	FRotator EyeRotation;
 
 	FCollisionObjectQueryParams ObjectQueryParams;
-	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	ObjectQueryParams.AddObjectTypesToQuery(CollisionChannel);
 
 	auto MyOwner = GetOwner();
 	MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
 
-	End = EyeLocation + (EyeRotation.Vector() * 1000);
+	End = EyeLocation + (EyeRotation.Vector() * TraceDistance);
 	// auto blockingHit = GetWorld()->LineTraceSingleByObjectType(Hit, EyeLocation, End, ObjectQueryParams);
 
 	TArray<FHitResult> Hits;
 	FCollisionShape Shape;
-	auto Radius = 30.f;
-	Shape.SetSphere(Radius);
-	auto blockingHit = GetWorld()->SweepMultiByObjectType(Hits, EyeLocation, End, FQuat::Identity, ObjectQueryParams,
-	                                                      Shape);
+	Shape.SetSphere(TraceRadius);
+	auto blockingHit = GetWorld()->SweepMultiByObjectType(Hits, EyeLocation, End, FQuat::Identity,
+	                                                      ObjectQueryParams, Shape);
 
 
+	FocusedActor = nullptr;
 	FColor Color = blockingHit ? FColor::Green : FColor::Red;
 	for (FHitResult Hit : Hits)
 	{
 		auto HitActor = Hit.GetActor();
-		if(bDebugDraw)
-			DrawDebugSphere(GetWorld(), Hit.ImpactPoint, Radius, 32, Color, false, 2.f);
+		if (bDebugDraw)
+			DrawDebugSphere(GetWorld(), Hit.ImpactPoint, TraceRadius, 32, Color, false, 2.f);
 
 		if (!HitActor || !HitActor->Implements<USGameplayInterface>()) continue;
 
-		ISGameplayInterface::Execute_Interact(HitActor, Cast<APawn>(MyOwner));
-		blockingHit = true;
+		FocusedActor = HitActor;
 		break;
 	}
-	if(bDebugDraw)
+
+	if (FocusedActor)
+	{
+		if (DefaultWidgetInstance == nullptr && ensure(DefaultWidgetClass))
+		{
+			DefaultWidgetInstance = CreateWidget<USWorldUserWidget>(GetWorld(), DefaultWidgetClass);
+		}
+
+		if (DefaultWidgetInstance)
+		{
+			DefaultWidgetInstance->SetAttachedActor(FocusedActor);
+			if (!DefaultWidgetInstance->IsInViewport())
+			{
+				DefaultWidgetInstance->AddToViewport();
+			}
+		}
+	}
+	else
+	{
+		if (DefaultWidgetInstance)
+		{
+			DefaultWidgetInstance->RemoveFromParent();
+		}
+	}
+	if (bDebugDraw)
 		DrawDebugLine(GetWorld(), EyeLocation, End, Color,
-	              false, 2.f, 0, 2.f);
+		              false, 2.f, 0, 2.f);
+}
+
+void USInteractionComponent::PrimaryInteract()
+{
+	if (FocusedActor == nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, "No Focus Actor to Interact with");
+		return;
+	}
+
+
+	ISGameplayInterface::Execute_Interact(FocusedActor, Cast<APawn>(GetOwner()));
 }
--- Source/TomLoomanCourse/Public/SInteractionComponent.h
+++ Source/TomLoomanCourse/Public/SInteractionComponent.h
@@ -1,11 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "SInteractionComponent.generated.h"
 
+
+class USWorldUserWidget;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TOMLOOMANCOURSE_API USInteractionComponent : public UActorComponent
@@ -15,17 +15,35 @@ class TOMLOOMANCOURSE_API USInteractionComponent : public UActorComponent
 public:
 	void PrimaryInteract();
 	
-public:	
-	// Sets default values for this component's properties
 	USInteractionComponent();
 
 protected:
-	// Called when the game starts
+	
+	UPROPERTY()
+	TObjectPtr<AActor> FocusedActor;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<USWorldUserWidget> DefaultWidgetClass;
+	
+	UPROPERTY()
+	TObjectPtr<USWorldUserWidget> DefaultWidgetInstance;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Trace")
+	float TraceDistance;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Trace")
+	float TraceRadius;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Trace")
+	TEnumAsByte<ECollisionChannel> CollisionChannel;
+	
+	void FindBestInteractable();
+	
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+
+	
 };
