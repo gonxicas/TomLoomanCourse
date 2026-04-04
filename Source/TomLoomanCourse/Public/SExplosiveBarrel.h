9fca74e3250fe4793ddb8f7ab8ccf18e5eb8aff7
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-04-04 23:22:53 +0900

    feat: replicate explosive barrel

--- Source/TomLoomanCourse/Public/SExplosiveBarrel.h
+++ Source/TomLoomanCourse/Public/SExplosiveBarrel.h
@@ -28,7 +28,7 @@ protected:
 	
 	// Called when the game starts or when spawned
 	virtual void PostInitializeComponents() override;
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable)
 	void FireImpulse(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 public:	
