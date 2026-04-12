ddd7fa23b4e6788f5bb01d1b6a1d3a490453e29e
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-04-12 21:06:01 +0900

    feat: add stunned animation

---
+++ Source/TomLoomanCourse/Private/SAnimInstance.cpp
@@ -0,0 +1,24 @@
+﻿#include "SAnimInstance.h"
+
+#include "GameplayTagContainer.h"
+#include "Actions/SActionComponent.h"
+
+void USAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	if (const auto OwningActor = GetOwningActor())
+	{
+		ActionComponent = Cast<USActionComponent>(OwningActor->GetComponentByClass(USActionComponent::StaticClass()));
+	}
+}
+
+void USAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	static FGameplayTag StunnedTag = FGameplayTag::RequestGameplayTag("Status.Stunned");
+	if (ActionComponent)
+	{
+		bIsStunned = ActionComponent->ActiveGameplayTags.HasTag(StunnedTag);
+	}
+}
---
+++ Source/TomLoomanCourse/Public/SAnimInstance.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SAnimInstance.generated.h"
+
+class USActionComponent;
+
+UCLASS()
+class TOMLOOMANCOURSE_API USAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	bool bIsStunned;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	TObjectPtr<USActionComponent> ActionComponent;
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+};
